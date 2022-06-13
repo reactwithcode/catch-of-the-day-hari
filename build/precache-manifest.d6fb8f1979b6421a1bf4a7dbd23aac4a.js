bb67c23e67557fdd3cce0b2487dfb13c7578cc47
thohariakb <thohari.akbar@gmail.com>
2022-06-13 21:55:20 +0700

    config to ship it

---
+++ build/precache-manifest.d6fb8f1979b6421a1bf4a7dbd23aac4a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84bcea4b36ae6f5ba546d7d5a850be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17386593b62f488ab215",
+    "url": "/static/css/main.afa0cf84.chunk.css"
+  },
+  {
+    "revision": "e01659ba6691b78a2662",
+    "url": "/static/js/2.6d0aad15.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.6d0aad15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17386593b62f488ab215",
+    "url": "/static/js/main.405e8378.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.405e8378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f8e0c71d064422087a",
+    "url": "/static/js/runtime-main.241b17a7.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
